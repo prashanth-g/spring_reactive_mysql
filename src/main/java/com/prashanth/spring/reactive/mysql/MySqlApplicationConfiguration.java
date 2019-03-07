0452fb69aa021991c91d77ebaa4ef0b2a248188b
prashanth-g <prashanthcsgce@gmail.com>
2019-03-07 19:37:49 +0530

    Added Connection Configuration Class

---
+++ src/main/java/com/prashanth/spring/reactive/mysql/MySqlApplicationConfiguration.java
@@ -0,0 +1,24 @@
+package com.prashanth.spring.reactive.mysql;
+
+import com.github.jasync.r2dbc.mysql.JasyncConnectionFactory;
+import com.github.jasync.sql.db.mysql.pool.MySQLConnectionFactory;
+import com.github.jasync.sql.db.mysql.util.URLParser;
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+@EnableR2dbcRepositories
+public class MySqlApplicationConfiguration extends AbstractR2dbcConfiguration {
+
+    @Override
+    public ConnectionFactory connectionFactory() {
+        String url = "mysql://user:@localhost:3306/orders";
+        return new JasyncConnectionFactory(new MySQLConnectionFactory(
+                URLParser.INSTANCE.parseOrDie(url, StandardCharsets.UTF_8)
+        ));
+    }
+}

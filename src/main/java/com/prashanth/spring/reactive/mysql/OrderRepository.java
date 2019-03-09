5895cd6ab45d5b9c5e388aab02d590be45d46b42
prashanth-g <prashanthcsgce@gmail.com>
2019-03-09 21:18:11 +0530

    Updated Order Repo

--- src/main/java/com/prashanth/spring/reactive/mysql/OrderRepository.java
+++ src/main/java/com/prashanth/spring/reactive/mysql/OrderRepository.java
@@ -2,5 +2,7 @@ package com.prashanth.spring.reactive.mysql;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public class OrderRepository {
+
+public interface OrderRepository extends ReactiveCrudRepository<Order, Long>{
+
 }
